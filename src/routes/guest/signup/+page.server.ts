7043d9e43a404546e5cd301b1a5094c759fc4935
Patrik <pataxe02@edu.umea.se>
2024-02-07 12:44:43 +0100

    login

---
+++ src/routes/guest/signup/+page.server.ts
@@ -0,0 +1,58 @@
+import { fail, redirect, type Cookies, error } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+import { prisma } from '$lib';
+import { randomBytes, pbkdf2Sync } from 'crypto';
+
+function hashPassword(password:string) {
+    const salt = randomBytes(16).toString('hex');
+    const hash = pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    return { salt, hash };
+}
+
+function validatePassword(inputPassword:string, storedSalt:string, storedHash:string) {
+    const hash = pbkdf2Sync(inputPassword, storedSalt, 1000, 64, 'sha512').toString('hex');
+    return storedHash === hash;
+}
+
+export const load = (async ({cookies}) => {
+    let username = cookies.get('username')
+    if (username){
+        console.log('signup')
+        throw redirect(307, '/')
+    }
+    return {};
+}) satisfies PageServerLoad;
+
+
+export const actions: Actions = {
+    signup: async ({request, cookies})=> {
+
+        let data = await request.formData();
+        let username = data.get("username")?.toString();
+        let password = data.get('password')?.toString();
+        let userColor = data.get('userColor')?.toString();
+
+        if(!username){
+            return fail(404, {username:'Please submit a username'})
+        }
+        if(!password){
+            return fail(404, {username:'Please submit a password'})
+        }
+
+        let user = await prisma.user.findUnique({where:{name:username}})
+
+        if(user){
+            return fail(400, {username:'Username is taken'})
+        }
+        const { salt, hash } = hashPassword(password);
+        await prisma.user.create({
+            data: 
+            {name: username, password: hash, salt: salt, color: userColor? userColor: '#000000'}
+            
+        })
+
+        cookies.set('username', username,{path: '/' , secure: false, expires: new Date(2044,10,12)});
+        throw redirect(307, '/');
+
+    }
+}
---
+++ src/routes/guest/signup/+page.svelte
@@ -0,0 +1,20 @@
+
+<script lang="ts">
+    import { enhance } from "$app/forms";
+    
+    export let form;
+
+</script>
+
+<form method="post" action="?/signup" use:enhance>
+
+    <label for="username">Username</label>
+    <input type="text" name ="username">
+    <label for="password">Password</label>
+    <input type="password" name='password'>
+    <button>Sign Up</button>
+    <input type="color" id="color_picker" class="color_picker" name="userColor"/>
+    {#if form?.username}
+        <span>{form.username}</span>
+    {/if}
+</form>
